62079a4115f8f912eac065c013e9ce2e6bb02e76
aniketjadhav1812 <128202035+aniketjadhav1812@users.noreply.github.com>
2025-08-10 19:06:02 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ configDotenv();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://moody-player-frontend-6pvd.onrender.com/",
   methods: ["GET", "POST"],
   credentials: true,
 }));
